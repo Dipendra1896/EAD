740bea79c6209551d5ce487bef72dea3c6d974bf
Dipendra1896 <dipendrapatel371896@gmail.com>
2025-05-24 17:15:59 +0545

    Factory design pattern implemented

--- Lab-01/Singleton/src/FactoryDemo.java
+++ Lab-01/Singleton/src/FactoryDemo.java
@@ -6,14 +6,14 @@ interface Animal {
 // Concrete Product 1
 class Dog implements Animal {
     public void speak() {
-        System.out.println("Woof!");
+        System.out.println("This is sound of a dog:Woof!");
     }
 }
 
 // Concrete Product 2
 class Cat implements Animal {
     public void speak() {
-        System.out.println("Meow!");
+        System.out.println(" This is sound of a cat:Meow!");
     }
 }
 
@@ -33,9 +33,9 @@ class AnimalFactory {
 public class FactoryDemo {
     public static void main(String[] args) {
         Animal dog = AnimalFactory.getAnimal("dog");
-        dog.speak(); // Output: Woof!
+        dog.speak(); 
 
         Animal cat = AnimalFactory.getAnimal("cat");
-        cat.speak(); // Output: Meow!
+        cat.speak(); 
     }
 }
